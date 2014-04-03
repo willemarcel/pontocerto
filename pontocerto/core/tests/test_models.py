bea2d46605466837bfb636299a615615e3899479
Wille Marcel <wille@wille.blog.br>
2014-04-02 22:22:53 -0300

    adiciona teste do core/models

---
+++ pontocerto/core/tests/test_models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+from django.contrib.gis.geos import Point
+
+from ..models import Ponto
+
+
+class CreatePontoTest(TestCase):
+
+    def setUp(self):
+        ponto = Ponto(osmid=123, location=Point(1.34, 2.45))
+        ponto.save()
+
+    def test_unicode(self):
+        self.assertEqual(Ponto.objects.get(osmid=123).__unicode__(),
+                            str(Ponto.objects.get(osmid=123).id))
