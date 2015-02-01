77fbf7e829506331e3d72208121d80f604b86f5c
Wille Marcel <wille@wille.blog.br>
2015-02-01 16:19:30 -0200

    update django, rest-framework and fix some details

--- pontocerto/core/urls.py
+++ pontocerto/core/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import patterns, url
 
 from .views import GeojsonPontoList
 
-
 urlpatterns = patterns('',
     url(r'^pontos.geojson/$', GeojsonPontoList.as_view(),
          name='api_geojson_ponto_list'),
