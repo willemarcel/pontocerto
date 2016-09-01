fd22b3848f966822ee3f7b3c2a400e5ec4aadf14
Wille Marcel <wille@wille.blog.br>
2016-09-01 14:04:27 -0300

    add filter to AvaliacaoAdmin page

--- pontocerto/core/admin.py
+++ pontocerto/core/admin.py
@@ -7,5 +7,11 @@ class PontoAdmin(admin.OSMGeoAdmin):
     search_fields = ['id', 'osmid', 'nome']
     list_display = ('__str__', 'osmid', 'nome')
 
+
+class AvaliacaoAdmin(admin.OSMGeoAdmin):
+    search_fields = ['ponto__id', 'ponto__nome']
+    list_display = ['id', 'ponto', 'final']
+
+
 admin.site.register(Ponto, PontoAdmin)
-admin.site.register(Avaliacao)
+admin.site.register(Avaliacao, AvaliacaoAdmin)
